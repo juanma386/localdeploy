4ab1029fc0ba61fa40ad247b8935ee354746c849
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2019-03-06 01:41:41 -0300

    Primer despliegue linux php 5.6

---
+++ bin/server.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+echo '
+## Part Deploy working
+##
+## @author      Villalba Juan Manuel Pedro <https://github.com/juanma386>
+
+# @Version:     0.0.1
+# @Estado:      Alpha
+# @Destino:     Deploy Server Php Linux
+# @Date:        05:41pm 14-09-2017
+# @Advertencia  Usar con cautela y bajo propia responsabilidad
+# @Licence:     GPLv2 Free Software Foundation <licensing@fsf.org>
+';
+cd `dirname $0`/..
+_hoy=$(date +"%m_%d_%Y_%r_%S")
+_pwd=$(pwd)
+
+file=./testing.server.log
+if [ ! -e "$file" ]; then
+    echo "File does not exist"
+    echo "Create File inicializated Deploy"." [ "$_hoy" ]"
+    echo "Create File inicializated Deploy"." [ "$_hoy" ]" > testing.server.log
+else
+    echo "Checking file is exist deployed again"." [ "$_hoy" ]"
+    echo "Deployed again executed "." [ "$_hoy" ]" >> testing.server.log
+fi
+
+ADDR_PORT=${1:-127.0.0.1:8000}
+DOC_ROOT="$_pwd"
+
+php5.6 -S "$ADDR_PORT" -t "$DOC_ROOT/" -f bin/route.php
